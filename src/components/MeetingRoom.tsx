0db3c3fc725072323fa58b37b7dbba0fba7b6afe
Vasantkr97 <vasantkr97@gmail.com>
2025-02-17 08:39:27 +0530

    SC

---
+++ src/components/MeetingRoom.tsx
@@ -0,0 +1,76 @@
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from './ui/resizable';
+import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu';
+import { Button } from './ui/button';
+import { LayoutListIcon, UserIcon } from 'lucide-react';
+
+const MeetingRoom = () => {
+  const router = useRouter();
+  const [layout, setLayout] = useState<"grid" | "speaker">("speaker");
+  const [showParticipants, setShowParticipants] = useState(false);
+
+  return (
+    <div className='h-[calc(100vh-4rem-1px)]'>
+      <ResizablePanelGroup direction='horizontal'>
+        <ResizablePanel defaultSize={35} minSize={25} maxSize={100} className='relative' >
+          {/* VIDEO LAYOUT */}
+          <div className='absolute inset-0'>
+            {layout === "grid" ? <PaginatedGridLayout/> : <SpeakerLayout/>} 
+
+            {/* PARTICIPANTS LIST OVERLAY */}
+            {showParticipants && (
+              <div className='absolute right-0 top-0 h-full w-[300px] bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
+                <CallParticipantsList onClose={() => setShowParticipants(false)}/>
+              </div>
+            )}
+          </div>
+
+          {/* VIDEO CONTROLS */}
+          <div className='absolute bottom-4 left-0 right-0'>
+            <div className='flex flex-col items-center gap-4'>
+              <div className='flex items-center gap-2 flex-wrap justify-center px-4'>
+                <CallControls onLeave={() => router.push('/')} />
+
+                <div className='flex items-center gap-2'>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger>
+                      <Button variant='outline' size='icon' className='size-10'>
+                        <LayoutListIcon className='size-4' />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuItem onClick={() => setLayout("grid")}>
+                            Grid View
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setLayout("speaker")}>
+                            Speaker View
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+
+                  <Button
+                    variant='outline'
+                    size='icon'
+                    className='size-10'
+                    onClick={() => setShowParticipants(!showParticipants)}
+                  >
+                    <UserIcon className='size-4'  />
+                  </Button>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={65} minSize={25}>
+
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  )
+}
+
+export default MeetingRoom
