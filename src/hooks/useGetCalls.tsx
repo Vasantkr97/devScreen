dbf4650176544dbfc4f05ed8d5d105d742ce9077
Vasantkr97 <vasantkr97@gmail.com>
2025-02-18 14:40:17 +0530

    recording page

---
+++ src/hooks/useGetCalls.tsx
@@ -0,0 +1,55 @@
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useEffect, useState } from "react";
+
+
+const useGetCalls = () => {
+    const { user } = useUser();
+    const client = useStreamVideoClient();
+    const [calls, setIsCalls] = useState<Call[]>();
+    const [isLoading, setIsLoading] = useState(false);
+    console.log("stream Client: ",client)
+
+    useEffect(() => {
+        const loadCalls = async () => {
+            if (!client || !user?.id) return;
+
+            setIsLoading(true);
+
+            try {
+                const { calls } = await client.queryCalls({
+                    sort: [{ field: "starts_at", direction: -1 }],
+                    filter_conditions: {
+                        starts_at: { $exists: true },
+                        $or: [{ created_by_user_id: user.id }, { members: { $in: [user.id]}}]
+                    }
+                })
+                console.log("calls in useGEt CAlls:", calls)
+                setIsCalls(calls); 
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setIsLoading(false)
+            }
+        };
+        loadCalls();
+    }, [client, user?.id]);
+
+    const now = new Date();
+
+    const endedCalls = calls?.filter(({ state: { startsAt, endedAt }}: Call) => {
+        return (startsAt && new Date(startsAt) < now) || !!endedAt;
+    });
+
+    const upComingCalls = calls?.filter(({ state: { startsAt }}: Call ) => {
+        return startsAt && new Date(startsAt) > now;
+    });
+
+    const liveCalls = calls?.filter(({ state: { startsAt, endedAt }}: Call) => {
+        return startsAt && new Date(startsAt) < now && !endedAt
+    })
+
+    return { calls, endedCalls, upComingCalls, liveCalls, isLoading};
+};
+
+export default useGetCalls;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,96 @@
+import { interviews, User } from "@prisma/client"
 import { clsx, type ClassValue } from "clsx"
+import { addHours, intervalToDuration, isAfter, isBefore, isWithinInterval } from "date-fns";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
+};
+
+type Interview = interviews;
+type Users = User;
+
+export const groupInterviews = (interviews: Interview[]) => {
+  if (!interviews) return {};
+
+  return interviews.reduce((acc: any, interview: Interview) => {
+    const date = new Date(interview.startTime*1000);
+    const now = new Date();
+
+    if (interview.status === "succeeded") {
+      acc.succeeded = [...acc(acc.succeeded || []), interview];
+    } else if (interview.status === "failed") {
+      acc.failed = [...acc(acc.failed || []), interview];
+    } else if (isBefore(date, now)) {
+      acc.completed = [...acc(acc.completed || []), interview]
+    } else if (isAfter(date, now)) {
+      acc.upcoming = [...acc(acc.upcoming || []), interview]
+    }
+
+    return acc
+  },[])
+};
+
+export const getCandidateInfo = (users: Users[], candidateId: string) => {
+  const candidate = users?.find((user) => user.id === candidateId);
+
+  return {
+    name: candidate?.name || "Unknown Candidate",
+    image: candidate?.image || "",
+    initials:
+      candidate?.name
+        ?.split(" ")
+        .map((n: any) => n[0])
+        .join("") || "UC",
+  };
+};
+
+export const getInterviewerInfo = async (users: Users[], interviewerId: string) => {
+  const interviewer = users?.find((user) => user.clerkId === interviewerId);
+  return {
+    name: interviewer?.name || "Unknown Interviewer",
+    image: interviewer?.image,
+    initials:
+      interviewer?.name
+        ?.split(" ")
+        .map((n: any) => n[0])
+        .join("") || "UI",
+  };
+};
+
+
+export const calculateRecordingDuration = (startTime: string, endTime: string) => {
+  const start = new Date(startTime);
+  const end = new Date(endTime);
+
+  const duration = intervalToDuration({ start, end });
+
+  if (duration.hours && duration.hours > 0) {
+    return `${duration.hours}:${String(duration.minutes).padStart(2, "0")}:${String(
+      duration.seconds
+    ).padStart(2, "0")}`;
+  }
+
+  if (duration.minutes && duration.minutes > 0) {
+    return `${duration.minutes}:${String(duration.seconds).padStart(2, "0")}`;
+  }
+
+  return `${duration.seconds} seconds`;
+};
+
+
+export const getMeetingStatus = (interview: Interview) => {
+  const now = new Date();
+  const interviewStartTime = interview.startTime;
+  const endTime = addHours(interviewStartTime, 1);
+
+  if (
+    interview.status === "completed" ||
+    interview.status === "failed" ||
+    interview.status === "succeeded"
+  )
+    return "completed";
+  if (isWithinInterval(now, { start: interviewStartTime, end: endTime })) return "live";
+  if (isBefore(now, interviewStartTime)) return "upcoming";
+  return "completed";
+};
